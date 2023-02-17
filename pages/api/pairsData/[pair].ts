f6ad604ce98408ef46245b8ad47d8969596744ab
Giuseppe Ippolito <gippolito@hotmail.co.uk>
2023-02-17 07:09:11 +0000

    minor bug fixed

--- pages/api/pairsData/[pair].ts
+++ pages/api/pairsData/[pair].ts
@@ -5,13 +5,14 @@ export default async (req: NextApiRequest, res: NextApiResponse<LineGraphData>) 
     const { query: { pair } } = req;
   
     try {
-      const raw = await fetch(`https://www.bitstamp.net/api/v2/ticker/${pair}`, { mode: 'no-cors' });
+      const raw = await fetch(`https://www.bitstamp.net/api/v2/ticker/${pair}`);
       const data = await raw.json();
       const obj = {
         timestamp: data.timestamp,
         last: data.last,
       };
   
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Content-Type', 'application/json');
       res.statusCode = 200;
       res.end(JSON.stringify(obj));
@@ -22,7 +23,6 @@ export default async (req: NextApiRequest, res: NextApiResponse<LineGraphData>) 
     }
   };
   
-
 
 
   
